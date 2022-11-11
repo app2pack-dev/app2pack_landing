b4541768798457ce001297e516c44d6d52427e58
imaNNeoFighT <iman.neofight@gmail.com>
2022-11-11 23:21:19 +0330

    Remove # from url

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:app2pack_landing/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  // Here we set the URL strategy for our web app.
+  // It is safe to call this function when running on mobile or desktop as well.
+  setPathUrlStrategy();
   runApp(const MyApp());
 }
 
