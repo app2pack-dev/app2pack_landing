00811539386955eb386bda135b1139f2928b8b08
imaNNeoFighT <iman.neofight@gmail.com>
2022-11-11 23:27:04 +0330

    Update name

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'App 2 pack',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
